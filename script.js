246e17215ba5aa30e14ade1a0eb126145c5872d2
drafier <drafier.ai@gmail.com>
2026-03-16 23:04:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(p => p.classList.remove('active'));
+    document.getElementById(pageId).classList.add('active');
+    window.scrollTo(0,0);
+}
+
+function checkPassword() {
+    const input = document.getElementById('pwInput').value;
+    if (input === "fier") {
+        showPage('page4');
+        document.getElementById('pwInput').value = ""; 
+    } else {
+        alert("Fout wachtwoord! Probeer het opnieuw.");
+    }
+}
